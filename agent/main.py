b3e21b43780898e9df91e0ad95d060754b634563
Hector A. Valdecantos <hvaldecantos@gmail.com>
2026-05-25 17:25:40 -0300

    add loop for QA

--- agent/main.py
+++ agent/main.py
@@ -1,10 +1,28 @@
 from agent_graph import AgentState, agent
 from langchain_core.messages import HumanMessage
 from utils.graph_image import display_graph
+from vector_store import build_vectorstore
 
 display_graph(agent.get_graph(), filename="docs/rag_example_graph.png")
 
-result = agent.invoke(AgentState(
-    messages=[HumanMessage(content="What was the water consumption of AWS in 2021?")]))
+# build_vectorstore()
 
-print(result)
+
+def start_agent():
+    print("\n=== RAG AGENT===")
+
+    while True:
+
+        user_input = input("\nWhat is your question: ")
+        if user_input.lower() in ['exit', 'quit']:
+            break
+
+        messages = [HumanMessage(content=user_input)]
+        result = agent.invoke(AgentState(messages=messages))
+
+        print("\n=== ANSWER ===")
+        print(result['messages'][-1].content)
+
+
+if __name__ == "__main__":
+    start_agent()
